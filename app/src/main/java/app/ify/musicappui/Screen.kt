bb0d59d64adfc2e4409db747b11f23f59d06a395
waynetipsy <jacketoniru@gmail.com>
2024-04-17 18:14:51 +0100

    Music App UI

---
+++ app/src/main/java/app/ify/musicappui/Screen.kt
@@ -0,0 +1,23 @@
+package app.ify.musicappui
+
+import androidx.annotation.DrawableRes
+
+sealed class Screen(val title: String, val route: String ) {
+    sealed class DrawerScreen(val dTilte: String, val dRoute: String, @DrawableRes val icon: Int)
+        : Screen(dTilte, dRoute) {
+            object Account: DrawerScreen(
+                dTilte = "Account",
+                dRoute = "account",
+                R.drawable.ic_account
+            )
+         object Subscription: DrawerScreen(
+             dTilte = "Subscription",
+             dRoute = "subscribe",
+             R.drawable.ic_subacribe
+         )
+        object AddAccount: DrawerScreen(
+            dTilte = "Add Account",
+            dRoute = "add_account",
+            R.drawable.ic_baseline_person_add_alt_1_24
+        }
+}
